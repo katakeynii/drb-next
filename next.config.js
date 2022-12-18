a6cdace188cb25f035b6fd7b68457bda649ccfc3
Mohamed Camara Gueye <katakeynii@gmail.com>
2022-12-18 21:01:44 +0000

    adding redirection

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    async redirects() {
+    return [
+      {
+        source: '/inscription',
+        destination: 'https://bit.ly/drb-odc-24dec',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
