e8e8663c5b27c01246a9381f61dc7e33865d9877
Mohamed Camara Gueye <katakeynii@gmail.com>
2022-12-23 17:33:28 +0000

    adding live video

--- pages/live.js
+++ pages/live.js
@@ -11,8 +11,8 @@ const Live = () => {
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/mls-5MDzuY4"
-            title="YouTube video player"
+            src="https://www.youtube.com/5jry_hMHAws"
+            title="DRB MEETUP : 24 Decembre 2022 à Orange Digital Center"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
